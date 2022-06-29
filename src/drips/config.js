2e8ad72b38b72ea492d290dc600f8b5d2c7d2be6
Fadli128 <108339471+Fadli128@users.noreply.github.com>
2022-06-30 00:36:10 +0700

    Update config.js
    Signed-off-by: Fadli128 <108339471+Fadli128@users.noreply.github.com>

--- src/drips/config.js
+++ src/drips/config.js
@@ -37,12 +37,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','447441437150']
-global.premium = ['27634090203','447441437150']
-global.botname = 'ZIM BOT INC'
-global.ownername ='DRIPS'
-global.packname = 'ZIM BOT INC'
-global.quoted = 'https://www.youtube.com/c/DRIPSOFC'
+global.owner = ['994408465157','447441437150']
+global.premium = ['994408465157','447441437150']
+global.botname = 'AryBotz-MD'
+global.ownername ='Ayangnya Caa-!!'
+global.packname = 'AryBotz-MD'
+global.quoted = 'https://youtube.com/c/AryBotz'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -57,7 +57,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = 'AryBotz-MD'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
